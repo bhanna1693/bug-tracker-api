5c04c0ae7eec27fb2a831ce59940faa39bf37a54
brianhanna <bhanna1693@gmail.com>
2020-06-17 12:06:10 -0400

    master - first commit (Employee demo)

---
+++ src/main/java/com/bhanna/bugtracker/employee/DataInitializer.java
@@ -0,0 +1,21 @@
+package com.bhanna.bugtracker.employee;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataInitializer {
+    private static final Logger log = LoggerFactory.getLogger(DataInitializer.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Joe Exotic", "Tiger King")));
+            log.info("Preloading " + repository.save(new Employee("Carole Baskins", "That Bitch")));
+        };
+    }
+}
